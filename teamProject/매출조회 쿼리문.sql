bd67ecc3d6ca08ad9603cba97fc502ea20645464
hanYijin <gksdlwls123@naver.com>
2021-04-06 16:37:29 +0900

    ㅇㅇㅇ

---
+++ teamProject/매출조회 쿼리문.sql
@@ -0,0 +1,9 @@
+--SELET * FROM BUYING;
+--select *from buying;
+--SELEC
+--SELECT * FROM CUSTT * FROM CUSTOMER;
+select orderdate, pid,price, count from buying where to_char(orderdate,'yy/mm/dd')=TO_char(sysdate,'yy/mm/dd');
+
+--select id,name from customer where id =5;
+--select pid form buying; 
+select orderdate,  sum(price* count)  from buying group by orderdate having to_char(orderdate,'yy/mm/dd')=TO_char(sysdate,'yy/mm/dd');
