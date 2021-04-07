811a577492b85e267ea2b7e6eb187a5b6e3ebced
hanYijin <gksdlwls123@naver.com>
2021-04-07 16:19:29 +0900

    c#학습

--- teamProject/매출조회 쿼리문.sql
+++ teamProject/매출조회 쿼리문.sql
@@ -7,3 +7,5 @@ select orderdate, pid,price, count from buying where to_char(orderdate,'yy/mm/dd
 --select id,name from customer where id =5;
 --select pid form buying; 
 select orderdate,  sum(price* count)  from buying group by orderdate having to_char(orderdate,'yy/mm/dd')=TO_char(sysdate,'yy/mm/dd');
+
+select orderdate, sum(price* count) from buying group by orderdate having to_char(orderdate,'yy/mm/dd')=TO_char(sysdate,'yy/mm/dd');
